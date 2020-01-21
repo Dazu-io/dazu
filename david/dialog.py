ffbbf0f696c78dd958a0ab6026544284d79cb167
ralphg6 <raphaelcpinto@gmail.com>
2020-01-21 11:00:52 -0300

    fixes for pylint

--- david/dialog.py
+++ david/dialog.py
@@ -8,7 +8,7 @@ def fetch_dialog():
 
 def persist_dialog(data):
     with open('./data/dialog.json', 'w') as outfile:
-        json.dump(data, outfile);           
+        json.dump(data, outfile)
 
 def get_dialog_welcome(dialog_nodes):
     return dialog_nodes[0]
@@ -17,7 +17,7 @@ def get_dialog_anythinelse(dialog_nodes):
     return dialog_nodes[len(dialog_nodes) - 1]
 
 def evalCondition(condition, context, intent, entities):
-    return condition == '#' + intent;
+    return condition == '#' + intent
 
 class Dialog:
 
@@ -29,12 +29,12 @@ class Dialog:
 
     def dialog(self, input, context, intents, entities):
         if input == "":
-            return get_dialog_welcome(self.dialog_nodes);
+            return get_dialog_welcome(self.dialog_nodes)
         
         if len(intents) > 0:
-            intent = intents[0]["intent"];
+            intent = intents[0]["intent"]
             for dialog_node in self.dialog_nodes:
                 if evalCondition(dialog_node["condition"], context, intent, entities):
-                    return dialog_node;
+                    return dialog_node
         
         return get_dialog_anythinelse(self.dialog_nodes);
