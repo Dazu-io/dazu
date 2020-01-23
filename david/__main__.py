b07e5df9e7c5e01d97d985a0ac9b624c3f2f302c
ralphg6 <raphaelcpinto@gmail.com>
2020-01-23 17:06:03 -0300

    Update __main__.py for archtect refactor

--- david/__main__.py
+++ david/__main__.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from david.assistant import Assistant
 from david.googleadap import GoogleWebHook
 from david.dialog import fetch_dialog
-from david.brain import fetch_model, fetch_know
+#from david.brain import fetch_model, fetch_know
 
 app = Flask(__name__)
 CORS(app)
@@ -30,13 +30,13 @@ def google():
     data = request.get_json()
     return jsonify(googleWH.handle(data))  
 
-@app.route('/data/dialog')
-def data_dialog():
-    return jsonify(fetch_dialog())  
+#@app.route('/data/dialog')
+#def data_dialog():
+    #return jsonify(fetch_dialog())  
 
-@app.route('/data/know')
-def data_know():
-    return jsonify(fetch_know())           
+#@app.route('/data/know')
+#def data_know():
+    #return jsonify(fetch_know())           
 
 def main() -> None:
     app.run(host='0.0.0.0')
