d647655a73507b90237ba224b01a041c3b10df72
Raphael Pinto <raphaelcpinto@gmail.com>
2020-02-03 11:01:41 -0300

    Create registry

--- david/__main__.py
+++ david/__main__.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS
 
-from david.adapters.adapter import AdaptEngine
 from david.assistant import Assistant
 from david.dialog import fetch_dialog
 from david.googleadap import GoogleWebHook
+from david.registry import Registry
 
 # from david.brain import fetch_model, fetch_know
 
@@ -14,7 +14,7 @@ CORS(app)
 assistant = Assistant()
 googleWH = GoogleWebHook(assistant)
 
-adapt_engine = AdaptEngine()
+registry = Registry.get_instance()
 
 
 @app.route("/")
@@ -33,7 +33,7 @@ def dialog():
     requestData = request.get_json()
 
     # [TODO] receive adapter from query string
-    adapter = adapt_engine.getAdapter()
+    adapter = registry.getAdapter()
 
     if not adapter.validade_data(requestData):
         # [TODO] send status code 400
--- david/adapters/adapter.py
+++ david/adapters/adapter.py
@@ -34,28 +34,3 @@ class MessageAdapter(Adapter):
 
     def output(self, message: Message) -> Dict:
         return message.__dict__
-
-
-# [TODO] create a component registry and migrate this code
-class AdaptEngine:
-    adapters = {}
-
-    def __init__(self, defaultAdapter=None):
-
-        messageAdapter = MessageAdapter()
-        self.registryAdapter(messageAdapter)
-
-        if defaultAdapter:
-            self.defaultAdapter = defaultAdapter
-        else:
-            self.defaultAdapter = messageAdapter.name
-
-    def registryAdapter(self, adapter: Adapter):
-        self.adapters[adapter.name] = adapter
-
-    def getAdapter(self, adapterName=None):
-
-        if not adapterName:
-            adapterName = self.defaultAdapter
-
-        return self.adapters[adapterName]
