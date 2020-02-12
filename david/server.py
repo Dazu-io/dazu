61947414ea64756aa11116eb6510dd55e5931f6d
Raphael Pinto <raphaelcpinto@gmail.com>
2020-02-12 13:11:54 -0300

    Issue/51 document api (#65)
    * #51 Prepare swagger
    
    * #51 Reorganize routes

--- david/server.py
+++ david/server.py
@@ -1,53 +1,33 @@
 from sanic import Sanic
-from sanic.exceptions import abort
-from sanic.response import json, text
 
+from david import version
 from david.components.engine import Engine
 from david.config import DavidConfig
-from david.registry import Registry
-
-app = Sanic()
-
-
-@app.route("/")
-def hi(request):
-    text("Hi, am i David!")
-
-
-@app.route("/dialog", methods=["POST"])
-def dialog(request):
-    requestData = request.json
-
-    adapterName = request.get_args().get("adapter")
-    adapter = Registry.getAdapter(Server.config, adapterName)
-
-    if not adapter:
-        abort(400)
-        text("Invalid adapter")
-        return
-
-    if not adapter.validade_data(requestData):
-        abort(400)
-        text("Invalid input")
-        return
-
-    messageIn = adapter.input(requestData)
-    messageOut = Server.engine.respond(messageIn)
-    responseData = adapter.output(messageOut)
-    return json(responseData)
+from david.routes import router
 
 
 class Server:
 
-    config: DavidConfig = None
+    config: DavidConfig
 
-    engine: Engine = None
+    engine: Engine
 
     @classmethod
-    def prepare(cls, config: DavidConfig, engine: Engine):
+    def start(cls, config: DavidConfig, engine: Engine):
+
         cls.config = config
+
         cls.engine = engine
 
-    @classmethod
-    def start(cls) -> None:
+        app = Sanic()
+
+        # app.config['DAVID_CONFIG', config]
+        # app.config['DAVID_ENGINE', engine]
+
+        app.config["API_BASEPATH"] = "/api"
+        app.config["API_TITLE"] = "David"
+        app.config["API_VERSION"] = version.__version__
+
+        app.blueprint(router)
+
         app.run(host="0.0.0.0", port=5000)
