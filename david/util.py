362c1156c553cdafee44c66f970f72cc8cf8d186
Raphael Pinto <raphaelcpinto@gmail.com>
2020-02-12 11:52:55 -0300

    #24 Refactory forgotten code to SimpleNLU

--- david/util.py
+++ david/util.py
@@ -6,45 +6,6 @@ import nltk
 import unidecode
 
 
-def fetch_stopwords():
-    stopwords = set()
-    try:
-        stopwords = set(line.strip() for line in open("./data/stopwords.txt", "r"))
-    except Exception as e:
-        pass
-    return stopwords
-
-
-stopwords = fetch_stopwords()
-# stopwords = nltk.corpus.stopwords.words('portuguese')
-# print "stopwords",stopwords
-
-NON_CONTENT = r"[^\w\d\s]"
-
-
-def tokenize(sentence):
-    # print "sentence",sentence
-    # remove accents
-    sentence = unidecode.unidecode(sentence)
-    # print "sentence1",sentence
-    # remove non content
-    sentence = re.sub(NON_CONTENT, "", sentence)
-    # print "sentence2",sentence
-    # lower
-    sentence = sentence.lower()
-    # print "sentence3",sentence
-    # split
-    tokens = sentence.split(" ")
-
-    tokens = list(filter(lambda t: t not in stopwords, tokens))
-
-    tokens = list(filter(lambda t: len(t) > 0, tokens))
-
-    # print("tokens", tokens)
-
-    return tokens
-
-
 def json_to_string(obj: Any, **kwargs: Any) -> Text:
     indent = kwargs.pop("indent", 2)
     ensure_ascii = kwargs.pop("ensure_ascii", False)
